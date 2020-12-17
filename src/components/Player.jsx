dc01b34d4e40d77de3315d69c1da374de0161a06
omripeled <omripd@gmail.com>
2020-12-17 23:07:33 +0200

    changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactHlsPlayer from 'react-hls-player';
 
-const player = (props) => {
-    return (
-        <ReactHlsPlayer
-            url='https://s5.ipcamlive.com/streams/055fda518ee94a6c0/stream.m3u8'
-            autoplay={false}
-            controls={true}
-            width={500}
-            height={375}
-        />
-    );
+class player extends Component {
+
+    geURL1 = () => {
+        return 'https://edge08.nginx.hdontap.com/ho3/elporto_swellmagnet.stream/chunklist.m3u8'
+
+    }
+    geURL2 = () => {
+        return 'https://s5.ipcamlive.com/streams/055fda518ee94a6c0/stream.m3u8'
+    }
+
+    
+
+
+    render () {
+        return (
+            <div id="player1">
+                <ReactHlsPlayer
+                    url={this.geURL1()}
+                    autoplay={true}
+                    controls={true}
+                    width={500}
+                    height={375}
+                />
+            </div>
+        );
+    }
 }
 
 export default player;
