4911c839cd11540e70e283c19e54165ecade2d0b
omripeled <omripd@gmail.com>
2020-12-17 23:35:43 +0200

    changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,28 +2,37 @@ import React, { Component } from 'react';
 import ReactHlsPlayer from 'react-hls-player';
 
 class player extends Component {
-
-    geURL1 = () => {
-        return 'https://edge08.nginx.hdontap.com/ho3/elporto_swellmagnet.stream/chunklist.m3u8'
-
-    }
-    geURL2 = () => {
-        return 'https://s5.ipcamlive.com/streams/055fda518ee94a6c0/stream.m3u8'
+    state = {
+        url: 'https://edge08.nginx.hdontap.com/ho3/elporto_swellmagnet.stream/chunklist.m3u8'
     }
 
-    
-
+    renderLoadButton = (url, label) => {
+        return (
+            <button onClick={() => this.load(url)}>
+                {label}
+            </button>
+        )
+    }
+    load = url => {
+        this.setState({
+            url
+        })
+    }
 
     render () {
+        const { url } = this.state
         return (
             <div id="player1">
-                <ReactHlsPlayer
-                    url={this.geURL1()}
+                <ReactHlsPlayer id='op'
+                    url={url}
                     autoplay={true}
                     controls={true}
                     width={500}
                     height={375}
                 />
+
+                {this.renderLoadButton('https://edge08.nginx.hdontap.com/ho3/elporto_swellmagnet.stream/chunklist.m3u8', 'Spain')}
+                {this.renderLoadButton('https://s5.ipcamlive.com/streams/055fda518ee94a6c0/stream.m3u8', 'Tel Aviv')}
             </div>
         );
     }
